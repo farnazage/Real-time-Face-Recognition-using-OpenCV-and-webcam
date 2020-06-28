8825047b8c815f9fa72cdbf7fa4b053203315912
Farnaz <farnazage@gmail.com>
2020-06-27 21:21:23 -0600

    Update datagathering.py

--- datagathering.py
+++ datagathering.py
@@ -1,9 +1,10 @@
 #data gathering for each individul
+# make sure you are in the project directory
+#import os
+#os.chdir("c:/Users/.../project")
 
 import cv2
-import os
-
-Detection = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cam= cv2.VideoCapture(0)
 
 id = input('\n enter user id end press Enter: ')
@@ -12,15 +13,15 @@ while(True):
     _, img= cam.read()
     img= cv2.flip(img, 1) # Flip camera vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detection.detectMultiScale(gray,1.3,5 )
+    faces = detector.detectMultiScale(gray,1.3,5 )
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         count += 1
 
-        cv2.imwrite("datagathering/" + str(id) + '.' +  str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("data/" + str(id) + '.' +  str(count) + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) &  0xFF == ord('s'):
+    k = cv2.waitKey(100) &  0xFF == ord('s')
     if k == 10:
         break
     elif count >= 30: 
